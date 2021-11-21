76d7e652f004a897d8d846785a9e980fc0284711
zooloozoo <93867768+zooloozoo@users.noreply.github.com>
2021-11-21 18:50:40 +0630

    Update chats.py

--- AlexaSongBot/modules/chats.py
+++ AlexaSongBot/modules/chats.py
@@ -1,8 +1,8 @@
 from config import OWNER_ID
 from pyrogram import filters
-from AlexaSongBot import app
-from AlexaSongBot.mrdarkprince import get_arg
-from AlexaSongBot.sql.chat_sql import load_chats_list, remove_chat_from_db
+from OliverMusicBot(MM) import app
+from OliverMusicBot(MM) import get_arg
+from OliverMusicBot(MM).sql.chat_sql import load_chats_list, remove_chat_from_db
 from io import BytesIO
 
 
